79301deed0ff33acc7fb088426e1bdd17cb77adb
Antony <vontonnie@gmail.com>
2022-02-04 07:20:27 +0300

    Add ChipsSection Composable

--- app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
@@ -1,18 +1,25 @@
 package com.plcoding.meditationuiyoutube
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.plcoding.meditationuiyoutube.ui.theme.ButtonBlue
+import com.plcoding.meditationuiyoutube.ui.theme.DarkerButtonBlue
 import com.plcoding.meditationuiyoutube.ui.theme.DeepBlue
+import com.plcoding.meditationuiyoutube.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen() {
@@ -26,6 +33,7 @@ fun HomeScreen() {
 
         Column {
             GreetingSection()
+            ChipSection(chips = listOf("Sweet Sleep", "Insomnia", "Depression"))
         }
     }
 }
@@ -57,6 +65,46 @@ fun GreetingSection(name: String = "Tonnie") {
             )
         }
     }
+}
+
+
+@Composable
+fun ChipSection(chips: List<String>) {
+
+    var selectedChipIndex by remember { mutableStateOf(0) }
+
+    LazyRow {
+
+        /*  items(chips.size){ i->
+              selectedChipIndex+1
+              Surface(modifier = Modifier.padding(5.dp), shape = RoundedCornerShape(8.dp), elevation = 8.dp) {
+
+                  Text(text = chips[i], Modifier.padding(12.dp))
+              }
+          }*/
+
+        items(chips.size) { i ->
+
+
+            Box(
+                modifier = Modifier
+                        .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                        .clickable { selectedChipIndex = i }
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(if (selectedChipIndex == i) ButtonBlue   else DarkerButtonBlue)
+                        .padding(15.dp)
+
+
+            ) {
+
+
+                Text(text = chips[i], color= TextWhite)
+            }
+        }
+
+    }
+
+
 }
 
 val themes = listOf("Sweet Sleep", "Insomnia", "Depression")
