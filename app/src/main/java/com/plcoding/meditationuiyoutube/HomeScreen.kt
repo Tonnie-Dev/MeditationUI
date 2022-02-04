f2ec77a7682d6efe998c171539b74535b5331b7a
Antony <vontonnie@gmail.com>
2022-02-04 07:59:17 +0300

    Add Current Meditation

--- app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -16,10 +17,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.plcoding.meditationuiyoutube.ui.theme.ButtonBlue
-import com.plcoding.meditationuiyoutube.ui.theme.DarkerButtonBlue
-import com.plcoding.meditationuiyoutube.ui.theme.DeepBlue
-import com.plcoding.meditationuiyoutube.ui.theme.TextWhite
+import com.plcoding.meditationuiyoutube.ui.theme.*
 
 @Composable
 fun HomeScreen() {
@@ -33,7 +31,8 @@ fun HomeScreen() {
 
         Column {
             GreetingSection()
-            ChipSection(chips = listOf("Sweet Sleep", "Insomnia", "Depression"))
+            ChipSection(chips = listOf("Sweet Sleep", "Insomnia", "Depression", "Rejection"))
+            CurrentMeditation()
         }
     }
 }
@@ -44,6 +43,7 @@ fun GreetingSection(name: String = "Tonnie") {
     Row(
         modifier = Modifier
                 .fillMaxWidth()
+
                 .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
@@ -91,14 +91,14 @@ fun ChipSection(chips: List<String>) {
                         .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
                         .clickable { selectedChipIndex = i }
                         .clip(RoundedCornerShape(10.dp))
-                        .background(if (selectedChipIndex == i) ButtonBlue   else DarkerButtonBlue)
+                        .background(if (selectedChipIndex == i) ButtonBlue else DarkerButtonBlue)
                         .padding(15.dp)
 
 
             ) {
 
 
-                Text(text = chips[i], color= TextWhite)
+                Text(text = chips[i], color = TextWhite)
             }
         }
 
@@ -107,4 +107,44 @@ fun ChipSection(chips: List<String>) {
 
 }
 
-val themes = listOf("Sweet Sleep", "Insomnia", "Depression")
+@Composable
+fun CurrentMeditation() {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(LightRed)
+                .padding(vertical = 20.dp, horizontal = 15.dp)
+    ) {
+        Column() {
+            Text(text = "Daily Thought", style = MaterialTheme.typography.h2)
+            Text(
+                text = "Meditation • 3-10 min",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                   
+                    .clip(CircleShape)
+                    .background(ButtonBlue)
+                    .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_play),
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+    }
+
+}
